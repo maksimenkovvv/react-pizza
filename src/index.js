f08220191e9511c82bba871a66dbe7e82283ecd2
maksimenkovvv <urbangorod888@gmail.com>
2025-07-11 16:25:13 +0300

    добавление хуков

--- src/index.js
+++ src/index.js
@@ -5,9 +5,5 @@ import App from './App.jsx';
 import reportWebVitals from './reportWebVitals.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
 reportWebVitals();
